3a3fb334ce4947c67cb26e460f724d171951c1b5
Whuss88 <williamhussey95@gmail.com>
2024-11-19 12:49:15 -0500

    database seeded

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,7 +1,35 @@
 const prisma = require("../prisma");
 
 const seed = async () => {
-  // TODO: this function
+  const numRestaurants = 3
+  const numCustomers = 5
+  const numReservations = 8
+
+  const restaurants = Array.from({ length: numRestaurants }, (_, i) => ({
+    name: `Restaurants ${i + 1}`,
+  }));
+  await prisma.restaurant.createMany({ data: restaurants})
+
+  const customers = Array.from({ length: numCustomers}, (_, i) => ({
+    name: `Customer ${i + 1}`,
+    email: `customer${i +1}@foo.bar`,
+  }));
+  await prisma.customer.createMany({ data: customers})
+
+  for (i = 0; i < numReservations; i++) {
+    const partySize = 1 + Math.floor(Math.random() * 3);
+
+    const party = Array.from({ length: partySize }, () => ({
+      id: 1 + Math.floor(Math.random() * numCustomers),
+    }));
+    await prisma.reservation.create({
+      data: { 
+        date: new Date().toDateString(),
+        restaurantId: 1 + Math.floor(Math.random() * numRestaurants),
+        party: {connect: party },
+      },
+    });
+  }
 };
 
 seed()
--- server.js
+++ server.js
@@ -1,11 +1,33 @@
 const express = require("express");
 const app = express();
-const PORT = 3000;
+const PORT = 3002;
 
 app.use(require("morgan")("dev"));
 app.use(express.json());
 
-// TODO: POST /reservations
+const prisma = require("./prisma");
+app.post("/reservations", async (req, res, next) => {
+  try {
+    const { date, restaurantId, customerIds } = req.body;
+
+    const party = customerIds.map((id) => ({id: +id}));
+
+    const reservation = await prisma.reservation.create({
+      data: {
+        date,
+        restaurantId: +restaurantId,
+        party: { connect: party },
+      },
+      include: {
+        restaurant: true,
+        party: true,
+      },
+    });
+    res.status(201).json(reservation);
+  } catch(e) {
+    next(e);
+  }
+});
 
 app.use((req, res, next) => {
   next({ status: 404, message: "Endpoint not found." });
